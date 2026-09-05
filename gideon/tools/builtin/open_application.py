c57c36017fca0fa723075c0f7157b0fda2a9e9fd
KontataGood <vip.tinachev@mail.ru>
2026-09-05 23:28:14 +0300

    Add application launch tool

--- gideon/tools/builtin/open_application.py
+++ gideon/tools/builtin/open_application.py
@@ -1,32 +1,40 @@
+from typing import Any
+
 from gideon.platforms.base import Platform
 from gideon.tools.base import Tool
 
 
 class OpenApplicationTool(Tool):
 
-    name = "open_application"
-
-    description = (
-        "Opens an application on the user's computer."
-    )
-
-    parameters = {
-        "type": "object",
-        "properties": {
-            "application": {
-                "type": "string",
-                "description": (
-                    "Name or path of the application to open."
-                ),
-            }
-        },
-        "required": ["application"],
-    }
-
     def __init__(self, platform: Platform):
         self._platform = platform
 
-    def execute(self, application: str) -> str:
+    @property
+    def name(self) -> str:
+        return "open_application"
+
+    @property
+    def description(self) -> str:
+        return "Opens an application on the user's computer."
+
+    @property
+    def parameters(self) -> dict[str, Any]:
+        return {
+            "type": "object",
+            "properties": {
+                "application": {
+                    "type": "string",
+                    "description": (
+                        "Name or path of the application to open."
+                    ),
+                }
+            },
+            "required": ["application"],
+        }
+
+    def execute(self, **kwargs: Any) -> str:
+        application = kwargs["application"]
+
         self._platform.open_application(application)
 
         return f"Opened application: {application}"
